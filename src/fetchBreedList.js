5507e78cf676fd081f024082d1172c97236d2a80
Ygor Carmo <ygor.carmo@gmail.com>
2023-07-06 11:15:43 +1000

    fix: update http requests to https

--- src/fetchBreedList.js
+++ src/fetchBreedList.js
@@ -2,7 +2,7 @@ const fetchBreedList = async ({ queryKey }) => {
   const animal = queryKey[1];
   if (!animal) return [];
   const apiRes = await fetch(
-    `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+    `https://pets-v2.dev-apis.com/breeds?animal=${animal}`
   );
 
   if (!apiRes.ok) {
